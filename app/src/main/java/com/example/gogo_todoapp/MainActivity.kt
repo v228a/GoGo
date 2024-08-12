82ab3de9588de391fb7c6f7ab3af88561c65167a
vovka <v228a@proton.me>
2024-08-12 17:37:39 +0500

    Initial commit

---
+++ app/src/main/java/com/example/gogo_todoapp/MainActivity.kt
@@ -0,0 +1,33 @@
+ package com.example.gogo_todoapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.gogo_todoapp.ui.theme.GoGoTodoAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            GoGoTodoAppTheme {
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+
+                ) {
+
+                }
+            }
+        }
+    }
+}
+
+
